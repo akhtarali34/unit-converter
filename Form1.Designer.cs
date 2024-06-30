59fa04b2a0537dcbed4b910ba45ad4ddcc6deddb
akhtarali34 <akhtar4430@gmail.com>
2024-06-30 18:48:33 +0500

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace UnitConverterApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxType = new System.Windows.Forms.ComboBox();
+            this.comboBoxFrom = new System.Windows.Forms.ComboBox();
+            this.comboBoxTo = new System.Windows.Forms.ComboBox();
+            this.labelConvertedValue = new System.Windows.Forms.TextBox();
+            this.textBoxValue = new System.Windows.Forms.TextBox();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(152, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Conversion Type";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(152, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Convert";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(152, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Into";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(151, 274);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Converted Value";
+            // 
+            // comboBoxType
+            // 
+            this.comboBoxType.FormattingEnabled = true;
+            this.comboBoxType.Location = new System.Drawing.Point(356, 52);
+            this.comboBoxType.Name = "comboBoxType";
+            this.comboBoxType.Size = new System.Drawing.Size(121, 28);
+            this.comboBoxType.TabIndex = 4;
+            // 
+            // comboBoxFrom
+            // 
+            this.comboBoxFrom.FormattingEnabled = true;
+            this.comboBoxFrom.Location = new System.Drawing.Point(356, 99);
+            this.comboBoxFrom.Name = "comboBoxFrom";
+            this.comboBoxFrom.Size = new System.Drawing.Size(121, 28);
+            this.comboBoxFrom.TabIndex = 5;
+            // 
+            // comboBoxTo
+            // 
+            this.comboBoxTo.FormattingEnabled = true;
+            this.comboBoxTo.Location = new System.Drawing.Point(356, 152);
+            this.comboBoxTo.Name = "comboBoxTo";
+            this.comboBoxTo.Size = new System.Drawing.Size(121, 28);
+            this.comboBoxTo.TabIndex = 6;
+            // 
+            // labelConvertedValue
+            // 
+            this.labelConvertedValue.Location = new System.Drawing.Point(356, 268);
+            this.labelConvertedValue.Name = "labelConvertedValue";
+            this.labelConvertedValue.Size = new System.Drawing.Size(121, 26);
+            this.labelConvertedValue.TabIndex = 7;
+            // 
+            // textBoxValue
+            // 
+            this.textBoxValue.Location = new System.Drawing.Point(233, 99);
+            this.textBoxValue.Name = "textBoxValue";
+            this.textBoxValue.Size = new System.Drawing.Size(100, 26);
+            this.textBoxValue.TabIndex = 8;
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(132, 344);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(120, 33);
+            this.buttonConvert.TabIndex = 9;
+            this.buttonConvert.Text = "Covert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click_1);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(328, 344);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(111, 33);
+            this.buttonClear.TabIndex = 10;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click_1);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(504, 344);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(109, 33);
+            this.buttonExit.TabIndex = 11;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.textBoxValue);
+            this.Controls.Add(this.labelConvertedValue);
+            this.Controls.Add(this.comboBoxTo);
+            this.Controls.Add(this.comboBoxFrom);
+            this.Controls.Add(this.comboBoxType);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Unit Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxType;
+        private System.Windows.Forms.ComboBox comboBoxFrom;
+        private System.Windows.Forms.ComboBox comboBoxTo;
+        private System.Windows.Forms.TextBox labelConvertedValue;
+        private System.Windows.Forms.TextBox textBoxValue;
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace UnitConverterApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeComboBoxes();
+        }
+
+        private void InitializeComboBoxes()
+        {
+            // Add conversion types
+            comboBoxType.Items.AddRange(new string[] { "Length", "Weight" });
+            comboBoxType.SelectedIndex = 0;
+
+            // Populate units based on the selected conversion type
+            PopulateUnits(comboBoxType.SelectedItem.ToString());
+
+            // Handle the selected index change event for the conversion type ComboBox
+            comboBoxType.SelectedIndexChanged += ComboBoxType_SelectedIndexChanged;
+        }
+
+        private void ComboBoxType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Clear existing items
+            comboBoxFrom.Items.Clear();
+            comboBoxTo.Items.Clear();
+
+            // Populate units based on the selected conversion type
+            PopulateUnits(comboBoxType.SelectedItem.ToString());
+        }
+
+        private void PopulateUnits(string conversionType)
+        {
+            if (conversionType == "Length")
+            {
+                comboBoxFrom.Items.AddRange(new string[] { "Meters", "Kilometers", "Inches", "Feet" });
+                comboBoxTo.Items.AddRange(new string[] { "Meters", "Kilometers", "Inches", "Feet" });
+            }
+            else if (conversionType == "Weight")
+            {
+                comboBoxFrom.Items.AddRange(new string[] { "Grams", "Kilograms", "Pounds", "Ounces" });
+                comboBoxTo.Items.AddRange(new string[] { "Grams", "Kilograms", "Pounds", "Ounces" });
+            }
+
+            comboBoxFrom.SelectedIndex = 0;
+            comboBoxTo.SelectedIndex = 1;
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+      
+        {   
+
+            double value;
+            if (!double.TryParse(textBoxValue.Text, out value))
+            {
+
+                //
+                labelConvertedValue.Text = "Invalid input!";
+                //MessageBox.Show("PLEASE ENTER THE VALID POSITIVE INTEGER FOR GENERATION");
+                return;
+               
+            }
+
+            string fromUnit = comboBoxFrom.SelectedItem.ToString();
+            string toUnit = comboBoxTo.SelectedItem.ToString();
+            double result = ConvertUnits(value, fromUnit, toUnit);
+            labelConvertedValue.Text = result.ToString();
+        }
+
+
+
+       
+       
+
+        private double ConvertUnits(double value, string fromUnit, string toUnit)
+        {
+            if (fromUnit == toUnit)
+            {
+                return value;
+            }
+
+            // Conversion logic here
+            double valueInBaseUnit = value;
+            if (comboBoxType.SelectedItem.ToString() == "Length")
+            {
+                switch (fromUnit)
+                {
+                    case "Meters":
+                        valueInBaseUnit = value;
+                        break;
+                    case "Kilometers":
+                        valueInBaseUnit = value * 1000;
+                        break;
+                    case "Inches":
+                        valueInBaseUnit = value * 0.0254;
+                        break;
+                    case "Feet":
+                        valueInBaseUnit = value * 0.3048;
+                        break;
+                }
+
+                switch (toUnit)
+                {
+                    case "Meters":
+                        return valueInBaseUnit;
+                    case "Kilometers":
+                        return valueInBaseUnit / 1000;
+                    case "Inches":
+                        return valueInBaseUnit / 0.0254;
+                    case "Feet":
+                        return valueInBaseUnit / 0.3048;
+                }
+            }
+            else if (comboBoxType.SelectedItem.ToString() == "Weight")
+            {
+                switch (fromUnit)
+                {
+                    case "Grams":
+                        valueInBaseUnit = value;
+                        break;
+                    case "Kilograms":
+                        valueInBaseUnit = value * 1000;
+                        break;
+                    case "Pounds":
+                        valueInBaseUnit = value * 453.592;
+                        break;
+                    case "Ounces":
+                        valueInBaseUnit = value * 28.3495;
+                        break;
+                }
+
+                switch (toUnit)
+                {
+                    case "Grams":
+                        return valueInBaseUnit;
+                    case "Kilograms":
+                        return valueInBaseUnit / 1000;
+                    case "Pounds":
+                        return valueInBaseUnit / 453.592;
+                    case "Ounces":
+                        return valueInBaseUnit / 28.3495;
+                }
+            }
+
+            return 0;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void buttonConvert_Click_1(object sender, EventArgs e)
+        {
+            double value;
+            if (!double.TryParse(textBoxValue.Text, out value))
+            {
+
+                //
+                labelConvertedValue.Text = "Invalid input!";
+                //MessageBox.Show("PLEASE ENTER THE VALID POSITIVE INTEGER FOR GENERATION");
+                return;
+
+            }
+
+            string fromUnit = comboBoxFrom.SelectedItem.ToString();
+            string toUnit = comboBoxTo.SelectedItem.ToString();
+            double result = ConvertUnits(value, fromUnit, toUnit);
+            labelConvertedValue.Text = result.ToString();
+        }
+
+        private void buttonClear_Click_1(object sender, EventArgs e)
+        {
+            textBoxValue.Clear();
+            labelConvertedValue.Text = string.Empty;
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            // MessageBox.Show("PLEASE ENTER THE VALID POSITIVE INTEGER FOR GENERATION");
+            return;
+        }
+    }
+}
